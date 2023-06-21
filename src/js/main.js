d69ff3f41e3564cc4b362127aba6c701e844492f
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-06-21 12:17:58 +0200

    add lets

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 "use strict";
 
-let addCart = document.querySelector(".js-button");
+let minusBtn = document.querySelector(".js-minus");
+let plusBtn = document.querySelector(".js-plus");
+let numberInput = document.querySelector(".js-number");
+
+let userNumber = 0;
+
+function handleClick(ev) {
+  ev.preventDefault();
+  console.log("se presionó");
+}
+
+plusBtn.addEventListener("click", handleClick);
