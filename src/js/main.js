cc69bc90ecdbe9f6dc26d9c68ae0eacf3aae24e0
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-06-22 10:55:39 +0200

    add btnCart

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@ let minusBtn = document.querySelector(".js-minus");
 let plusBtn = document.querySelector(".js-plus");
 let numberInput = document.querySelector(".js-number");
 
+const addCart = document.querySelector(".js-cartBtn");
+const numberCart = document.querySelector(".js-numberCart");
+
 let userNumber = 0;
 
 function handleClickPlus(ev) {
@@ -22,5 +25,15 @@ function handleClickMinus() {
   console.log(userNumber);
 }
 
+function handleClickAdd() {
+  console.log();
+
+  // if (numberInput.value === numberCart) {
+  //   numberInput.style.display = "flex";
+  // } else {
+  //   numberInput.style.display = "none";
+  // }
+}
 plusBtn.addEventListener("click", handleClickPlus);
 minusBtn.addEventListener("click", handleClickMinus);
+addCart.addEventListener("click", handleClickAdd);
