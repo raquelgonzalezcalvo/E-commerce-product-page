3d195f932865656b7069cb818d397f6b9267163d
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-06-23 10:35:19 +0200

    add cartModal

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,9 @@ let numberInput = document.querySelector(".js-number");
 const addCart = document.querySelector(".js-cartBtn");
 const numberCart = document.querySelector(".js-numberCart");
 
+const iconBtn = document.querySelector(".js-icon");
+const cartModal = document.querySelector(".js-modal");
+
 let userNumber = 0;
 
 function handleClickPlus(ev) {
@@ -22,16 +25,21 @@ function handleClickMinus() {
     userNumber = 0;
   }
   numberInput.value = userNumber;
-  console.log(userNumber);
 }
 
 function handleClickAdd() {
   let lastValue = parseInt(numberCart.innerText);
   lastValue = lastValue + userNumber;
 
-  numberCart.innerText = userNumber;
+  numberCart.innerText = lastValue;
   numberCart.style.display = "block";
 }
+
+function handleClickIcon() {
+  cartModal.style.display = "block";
+}
+
 plusBtn.addEventListener("click", handleClickPlus);
 minusBtn.addEventListener("click", handleClickMinus);
 addCart.addEventListener("click", handleClickAdd);
+iconBtn.addEventListener("click", handleClickIcon);
